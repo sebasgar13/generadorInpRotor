e9260a68850856954c6aeab60b187fbc5e02c89f
sebasgar13 <sebastian.garciap@alumno.buap.mx>
2022-02-17 13:39:46 -0600

    programa terminado

--- main.dart
+++ main.dart
@@ -1,25 +1,43 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-void main(){
-  //num ocho = 8;
+void main(List<String> args){
+  print(args);
   void prueba() async {
-    final file = File('file.txt');
+    final file = File(args[0]);
     Stream<String> lines = file.openRead()
-      .transform(utf8.decoder)       // Decode bytes to UTF-8.
-      .transform(LineSplitter());    // Convert stream to individual lines.
+      .transform(utf8.decoder)      
+      .transform(LineSplitter());    
     try {
+      var file2 = File('template.inp');
+      var sink = file2.openWrite();
+      int primeto = 1;
       await for (var line in lines) {
-        for(int i = 0; i <10; i++){
-          print('$line: ${line.length} characters');
-          var file2 = File('file-${i*3}.txt');
-          var sink = file2.openWrite();
-          sink.write('$line: ${line.length} characters');
-          sink.write('FILE ACCESSED 2 ${DateTime.now()}\n');
-          // Close the IOSink to free system resources.
-          sink.close();
+        if(line.length > 3 && ((line[0]+line[1]+line[2]+line[3]) == args[1] || (line[0]+line[1]+line[2]+line[3]) == args[2] || (line[0]+line[1]+line[2]+line[3]) == args[3])){
+          print("Encontrado");
+        } else if(primeto == 1) {
+          sink.write(line);
+          primeto += 1;
+        } else {
+          sink.write("\n$line");
         }
       }
+      //Cuando se cierra el archivo esperamos que se escriba y lo utilizamos como template para escribir los otrso archivos
+      sink.close().then((value) => {
+        File('template.inp').readAsString().then((String contents) {
+          print(contents);
+          for(int i = 0; i < 12 ; i++){
+            var file2 = File('file-${i*30}.inp');
+            var sink = file2.openWrite();
+            sink.write('%chk=singleDAScanp-${i*30}.chk \n');
+            sink.write(contents);
+            sink.write('\n${args[2]}\t\t ${-240.0 + (i * 30)} \n');
+            sink.write('${args[3]}\t\t ${-120.0 + (i * 30)} \n \n');
+            sink.write('${args[1]}\t\t ${0.0 + (i * 30)} \n \n');
+            sink.close();
+          }
+        })
+      });
       print('File is now closed.');
     } catch (e) {
       print('Error: $e');
@@ -27,12 +45,4 @@ void main(){
   }
 
   prueba();
-  File('file.txt').readAsString().then((String contents) {
-    print(contents.length);
-  });
-  String encabezado = "Es una linea";
-  print(encabezado[3]+encabezado[4]);
-  for(int i =0; i < 10; i++){
-    print(" pala \t $i");
-  }
 }
